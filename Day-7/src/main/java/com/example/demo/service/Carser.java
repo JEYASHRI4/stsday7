c84c458714156e6a2f6e7e4ca617a8bac77b36d0
JEYASHRI4 <108671353+JEYASHRI4@users.noreply.github.com>
2023-08-25 15:33:33 +0530

    Add files via upload

---
+++ Day-7/src/main/java/com/example/demo/service/Carser.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Car;
+import com.example.demo.repository.Carrep;
+
+@Service
+public class Carser {
+@Autowired
+Carrep cre;
+public Car addCar(Car c) {
+	return cre.save(c);
+}
+public List<Car> showinfo(){
+	   return cre.findAll();
+}
+public List<Car> showowners(int owners){
+return cre.getowners(owners)	;
+}
+public List<Car> showaddress(String address){
+	return cre.getaddress(address);
+}
+public List<Car> showcarname(String carName){
+	return cre.getcarName(carName);
+}
+public List<Car> showcarownerandtype(int owner,String type){
+	return cre.getownersandcartype(owner, type);
+}
+public Car changeinfo(Car c) {
+	   return cre.saveAndFlush(c);
+}
+
+}
